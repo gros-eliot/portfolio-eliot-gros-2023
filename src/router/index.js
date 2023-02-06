265e3d4c656f5fee752ed185279678d3762b4ba6
gros-eliot <95086520+gros-eliot@users.noreply.github.com>
2023-02-06 18:13:52 +0100

    guard connexion

--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,16 @@
+// Fonction d'authentification
+import { getAuth } from 'https://www.gstatic.com/firebasejs/9.7.0/firebase-auth.js'
+
+// Fonctions Firestore
+import { 
+  getFirestore, 
+  collection, 
+  onSnapshot, 
+  query,
+  where
+} from '@/assets/js/firebase-firestore.js'
+
+
 import { createRouter, createWebHistory } from 'vue-router'
 import AccueilView from '../views/AccueilView.vue'
 import HomeView from '../views/HomeView.vue'
@@ -6,20 +19,57 @@ import ConnexionView from '../views/ConnexionView.vue'
 import FirebaseView from '../views/FirebaseView.vue'
 import TestFirebaseView from '../views/TestFirebaseView.vue'
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [ 
+const routes =  [ 
     { path: '/', name: 'AccueilView', component: AccueilView },
     { path: '/home', name: 'HomeView', component: HomeView },
 
-
-    
     { path: '/connexion', name: 'ConnexionView', component: ConnexionView },
-    { path: '/admin', name: 'FirebaseView', component: FirebaseView },
-    { path: '/admintest/:id', name: 'TestFirebaseView', component: TestFirebaseView },
+    { path: '/admin', name: 'FirebaseView', component: FirebaseView, beforeEnter:guardAdmin },
+    { path: '/admintest/:id', name: 'TestFirebaseView', component: TestFirebaseView, beforeEnter:guardAdmin },
 
-    
-  ]
+]
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+  scrollBehavior (to, from, savedPosition) {
+    return savedPosition || {top:0}
+  }
 })
+
+// On créé un guard : Observateur (fonction) permettant de savoir si un utilisateur a le droit d'utiliser une route
+// paramètres : to : d'où il vient, from où il veut aller, next où il doit aller après contrôle
+function guardAdmin(to, from, next) {
+  // recherche utilisateur connecté
+  getAuth().onAuthStateChanged(function(user) {
+    if(user) {
+      console.log(user);
+      // Obtenir Firestore
+      const firestore = getFirestore();
+      // Base de données collection user
+      const dbUser = collection(firestore, "user");
+      // Recherche du user par son uid
+      const q = query(dbUser, where("uid", "==", user.uid));
+      onSnapshot(q, (snapshot) => {
+          let userInfo = snapshot.docs.map(doc => ( {id:doc.id, ...doc.data()}));
+     
+          /*Création variable accessPage : si false => accès refusé. Si true => acces autorisé */
+          let accesPages;
+          accesPages=userInfo[0].admin;
+
+          if(accesPages === true){
+            next(to.params.name);
+            return;
+          }else{
+            // Non administrateur : retour accueil
+            alert("Page access error: you must be an administrator to access this page.");
+            next({name: "AccueilView"});
+            return;
+          }
+      })
+    }
+  });
+}
 
 export default router
