93d9452fa696b9e4cd848b467f0909378d16d634
gros-eliot <95086520+gros-eliot@users.noreply.github.com>
2023-03-05 17:38:33 +0100

    sketchs drawings imported

--- src/components/works/WORKPommsWorld.vue
+++ src/components/works/WORKPommsWorld.vue
@@ -5,6 +5,26 @@
     universe. You can also meet Mia, Eld, and Sharon. These are his friends.
   </p>
 
+  <h3>Sketchs</h3>
+
+  <img
+    src="/images/works-content-image/pomms-world-sketch-1.jpg"
+    alt="Pomms World : sketch"
+  />
+  <img
+    src="/images/works-content-image/pomms-world-sketch-2.jpg"
+    alt="Pomms World : sketch n2"
+  />
+
+  <h3>Sketchs with actions</h3>
+  <img src="" alt="" />
+  <img src="" alt="" />
+  <img src="" alt="" />
+  <img src="" alt="" />
+
+  <h3>Drawings on Fresco</h3>
+  <img src="" alt="" />
+  <img src="" alt="" />
   <img src="" alt="" />
   <img src="" alt="" />
 </template>
